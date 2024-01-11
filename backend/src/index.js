b92ef217d6654148350a266e3ef9dcfee286ac14
kwak <sal091625@gmail.com>
2024-01-11 16:35:25 +0900

    feat: express.static(), CORS 구현

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const cors = require('cors')
 const port = 4000;
 
+//다른 포트에서도 사용할 수 있도록 cors 선언
+app.use(cors());
+
 app.get('/', (req, res)=>{
-    res.send('안녕하세요');
+    res.send('안녕하세요!');
 });
+
+//uploads 폴더 내의 파일에 접근할 수 있게 함
+//path.join을 통해 절대경로로 설정함
+app.use(express.static(path.join(__dirname,'../uploads')));
 
 app.listen(port, ()=>{
     console.log(`${port}번에서 실행이 되었습니다.`)

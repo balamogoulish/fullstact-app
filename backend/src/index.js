2dd26a07460a7050cff3392e5402a55733269ad7
kwak <sal091625@gmail.com>
2024-01-11 16:41:41 +0900

    feat: express.json()

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,15 @@ const port = 4000;
 
 //다른 포트에서도 사용할 수 있도록 cors 선언
 app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('안녕하세요!');
+});
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+    res.json(req.body);
 });
 
 //uploads 폴더 내의 파일에 접근할 수 있게 함

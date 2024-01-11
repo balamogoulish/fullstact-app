65d7e490744ef4604c07ea7ca62adeec8fbe8dc8
kwak <sal091625@gmail.com>
2024-01-11 18:11:48 +0900

    feat: express에서 에러 처리하기

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,12 @@ app.get('/', (req, res)=>{
     res.send('안녕하세요!');
 });
 
+//에러 처리기
+app.use((error, req, res, next)=>{
+    res.status(err.status || 500); //error status 코드가 없다면, 서버 문제이므로 500을 전송함
+    res.send(error.message || '서버에서 에러가 발생했습니다.');
+})
+
 //uploads 폴더 내의 파일에 접근할 수 있게 함
 //path.join을 통해 절대경로로 설정함
 app.use(express.static(path.join(__dirname,'../uploads')));
